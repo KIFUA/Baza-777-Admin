627cbe3e82ea3bb6141b2767bc49f0e1d88ae50b
KIFUA <137715271+KIFUA@users.noreply.github.com>
2026-07-12 18:42:20 +0300

    build: migrate api entry point to esm
    Rename api/index.cjs to api/index.js and update vercel configuration to use standard esm module loading.

--- api/index.cjs
+++
@@ -1,3 +0,0 @@
-const serverApp = require("../dist/server.cjs");
-const app = serverApp.default || serverApp;
-module.exports = app;
---
+++ api/index.js
@@ -0,0 +1,5 @@
+import serverApp from "../dist/server.cjs";
+
+const app = serverApp.default || serverApp;
+
+export default app;
